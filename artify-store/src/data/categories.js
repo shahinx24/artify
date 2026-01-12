739709b08bae48877aef9631a69ebcaf853e97d1
Shahin <shahinofficial911@gmail.com>
2026-01-12 17:30:10 +0530

    message

--- artify-store/src/data/categories.js
+++ artify-store/src/data/categories.js
@@ -7,26 +7,26 @@ export const categories = [
   {
     id: "colors",
     name: "Colors",
-    image: "https://fastly.picsum.photos/id/25/5000/3333.jpg?hmac=yCz9LeSs-i72Ru0YvvpsoECnCTxZjzGde805gWrAHkM"
+    image: "/images/others/colors.png"
   },
   {
     id: "canvas",
     name: "Canvas",
-    image: "https://picsum.photos/300?random=12"
+    image: "/images/others/canvas.jpg"
   },
   {
     id: "sketchbooks",
     name: "Sketchbooks",
-    image: "https://picsum.photos/300?random=13"
+    image: "/images/others/sketchbooks.png"
   },
   {
     id: "markers",
     name: "Markers",
-    image: "https://picsum.photos/300?random=14"
+    image: "/images/others/markers.jpg"
   },
   {
     id: "craft",
     name: "Craft Supplies",
-    image: "https://picsum.photos/300?random=15"
+    image: "/images/others/craft.png"
   }
 ];
